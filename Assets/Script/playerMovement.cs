df49dae13317986b6d77f041e1bd15a5c3df14a9
bagustriwidi <=>
2020-03-19 20:14:29 +0700

    first commit

---
+++ Assets/Script/playerMovement.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float jump;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private int jumpValue;
+    [SerializeField] private Rigidbody2D rb;
+    private bool isGrounded;
+    private int midAirJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        midAirJump = jumpValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, layerMask);
+        if (isGrounded == true)
+        {
+            midAirJump = jumpValue;
+        }
+        if (midAirJump > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jump;
+            midAirJump--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && midAirJump == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jump;
+        }
+    }
+}
---
+++ Assets/lvl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvl : MonoBehaviour
+{
+    private const float playerDistance = 100f;
+    [SerializeField] private Transform startLvl;
+    [SerializeField] private Transform lvl1;
+    [SerializeField] private Transform lvl2;
+    [SerializeField] private Transform lvl3;
+    [SerializeField] private Transform lvl4;
+    [SerializeField] private Transform lvl5;
+    [SerializeField] private GameObject player;
+    private Vector3 lastEndPosition;
+    void Awake()
+    {
+        lastEndPosition = startLvl.Find("EndPosition").position;
+        spawn();
+    }
+    private void Update()
+    {
+
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < playerDistance)
+        {
+            spawn();
+        }
+
+    }
+    void spawn()
+    {
+        Transform[] listLvl = { lvl1, lvl2, lvl3, lvl4, lvl5 };
+        Transform randomPart = listLvl[Random.Range(0, listLvl.Length)];
+        Transform lastLvlPart = SpawnLvl(randomPart, lastEndPosition);
+        lastEndPosition = lastLvlPart.Find("EndPosition").position;
+    }
+    Transform SpawnLvl(Transform lvlPr, Vector3 spawnPosition)
+    {
+        Transform lvlPart = Instantiate(lvlPr, spawnPosition, Quaternion.identity);
+        return lvlPart;
+    }
+}
