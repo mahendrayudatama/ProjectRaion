75d2b4b3c8267b6023dbce53ea2dee5fc8a9b456
mahendrayudatama <mytama43@gmail.com>
2020-03-21 21:38:30 +0700

    refined lvl
    semua trigger + lvl destroyer/ despawner

---
+++ Assets/Script/LvlDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlDestroyer : MonoBehaviour
+{
+    private GameObject destroyer;
+
+    private void Start()
+    {
+        destroyer = GameObject.Find("Destroyer");
+    }
+
+    void Update()
+    {
+        if (transform.position.x < destroyer.transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
